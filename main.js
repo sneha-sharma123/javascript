9bdfcb52f26d87a1dbe366c75c74f3b2b2678f24
sneha-sharma123 <snehasharmaaa18@gmail.com>
2024-06-25 17:12:54 +0530

    javascript
    i am learning java script

---
+++ main.js
@@ -0,0 +1,14 @@
+let intergerNumber = 10;
+console.log(intergerNumber);
+
+let floatPointNumber = 5.5;
+console.log(floatPointNumber);
+
+let singleQuoteString = 'Hello World';
+console.log(singleQuoteString);
+
+let doubleQuoteString = "Hello World";
+console.log(doubleQuoteString);
+
+let name = 'world';
+let templateLitrealString =`Hello,${name}`
